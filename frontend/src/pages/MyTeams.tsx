3f8379192b20ec760ddf27e616a8cc1f9e912877
tusharb05 <tusharbaweja2005@gmail.com>
2025-09-30 16:12:34 +0530

    frontend update [skip ci]

--- frontend/src/pages/MyTeams.tsx
+++ frontend/src/pages/MyTeams.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useAuthContext } from "../context/AuthContext";
 import { Link } from "react-router-dom";
 import { FETCH_MY_TEAMS } from "../urls";
